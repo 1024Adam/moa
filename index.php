a5d3a3d83004467bc4f52fc3a75b355f7e5df854
1024Adam <adamjreid10@gmail.com>
2016-12-19 15:20:15 -0500

    Initial menu

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
 <?php
-echo 'Hello There!'
+echo '
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- generic meta -->
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
+    <!-- unique meta -->
+    <meta name="description" content="MOA - The Meal Organization Application">
+    <meta name="keywords" content="Adam,Reid,Meal,Organization,Planner">
+    <meta name="author" content="Adam Reid">
+    <title>MOA</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/navbar-static-top.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <!-- Static navbar -->
+    <div class="col-md-2">
+      <ul class="nav nav-pills nav-stacked">
+        <li role="presentation" class="active"><a href="#">Home</a></li>
+        <li role="presentation"><a href="#" class="nav-black-hover">Meals</a></li>
+        <li role="presentation"><a href="#" class="nav-blakc-hover">Grocery List</a></li>
+      </ul>
+    </div>
+
+    <div id="home" class="container">
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="./js/ie10-viewport-bug-workaround.js"></script>
+    <!--script defer async src="./js/adam.js"></script>-->
+  </body>
+</html>
+';
 ?>
